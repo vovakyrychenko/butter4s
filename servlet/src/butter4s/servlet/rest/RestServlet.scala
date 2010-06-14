59e4d9bf8470cac07ac8012c8868edf625f4d6e6
Vladimir Kirichenko <vladimir.kirichenko@gmail.com>
2010-06-14 17:43:03 +0300

    - WIP

--- servlet/src/butter4s/servlet/rest/RestServlet.scala
+++ servlet/src/butter4s/servlet/rest/RestServlet.scala
@@ -1,7 +1,6 @@
 package butter4s.servlet.rest
 
 import javax.servlet.http.HttpServletResponse
-import butter4s.servlet.{Response, Request, Servlet}
 import butter4s.servlet._
 import butter4s.reflect._
 
